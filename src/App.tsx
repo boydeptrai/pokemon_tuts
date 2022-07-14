cd43fd7bb72e0720e8a12620387df270da822406
Tuan Tran <langthamht047@gmail.com>
2022-07-13 21:18:25 -0700

    logic loading

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,41 @@ interface Pokemons {
 
 const App: React.FC = () => {
   
-  const [pokemons,setPokemons] = useState<Pokemon[]>([])
+  const [pokemons,setPokemons] = useState<Pokemon[]>([]);
+  const [nextUrl,setNextUrl] = useState<string>("");
+  const [loading,setLoading] = useState<boolean>(true)
   useEffect(() =>{
     const getPokemon = async () =>{
       const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20");
-
+      
+      setNextUrl(res.data.next);
       res.data.results.forEach(async (pokemon:Pokemons) =>{
         const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
 
         setPokemons((p) =>[...p,poke.data])
+        setLoading(false);
       })
     }
     getPokemon();
   },[])
+  const nextPage = async () =>{
+    setLoading(true);
+     let res = await axios.get(nextUrl);
+     setNextUrl(res.data.next);
+     res.data.results.forEach(async (pokemon:Pokemons) =>{
+       const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+       setPokemons((p) =>[...p,poke.data]);
+       setLoading(false);
+     })
+  }
   return (
     <div className="App">
       <div className="container">
         <header className="pokemon-header">Pokemon</header>
         <PokemonCollection pokemons={pokemons}/>
+        <div className="btn">
+          <button onClick={nextPage}>{loading ? "Loading.." : "Load More"}{""}</button>
+        </div>
       </div>
     </div>
   );
